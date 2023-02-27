e9b6a70d8db05f48351f3bbf92216259dc38b6e5
Paige Nelmes <designer@paigenelmes.com>
2023-02-27 15:57:06 -0800

    Add axios GET requests to scheduler-api

--- src/components/Panel.js
+++ src/components/Panel.js
@@ -6,8 +6,7 @@ class Panel extends Component {
 
     return (
       <section
-        className="dashboard__panel"
-        onClick={onSelect}>
+        className="dashboard__panel" onClick={onSelect}>
         <h1 className="dashboard__panel-header">{label}</h1>
         <p classname="dashboard__panel-value">{value}</p>
       </section>
