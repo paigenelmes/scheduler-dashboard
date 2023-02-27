d8c838629be2ed32051a49162a178d270f962f7c
Paige Nelmes <designer@paigenelmes.com>
2023-02-27 14:04:23 -0800

    Fix className in Dashboard, now inside main tag

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -37,19 +37,17 @@ class Dashboard extends Component {
       return <Loading />;
     }
 
-    const panels = data.map(panel => (
+    const panels = (this.state.focused ? data.filter(panel => this.state.focused === panel.id) : data)
+      .map(panel => (
       <Panel
         key={panel.id}
         id={panel.id}
         label={panel.label}
         value={panel.value}
       />
-    ))
+   ));
 
-    return <main>
-      className={dashboardClasses}
-      {panels}
-    </main>
+    return <main className={dashboardClasses}>{panels}</main>
   }
 }
 
